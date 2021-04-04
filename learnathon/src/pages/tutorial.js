b3bd0abbceb4f5f8d9211d0c5e49f2c0e788ca23
LeftClik <samrut.gadde@gmail.com>
2021-04-04 17:47:55 -0500

    yes

--- learnathon/src/pages/tutorial.js
+++ learnathon/src/pages/tutorial.js
@@ -1,14 +1,24 @@
 import React, { useState, useEffect } from "react";
-import { Card, CardContent, Typography, ButtonBase, Grid, Button } from "@material-ui/core";
+import {
+  Card,
+  CardContent,
+  Typography,
+  ButtonBase,
+  Grid,
+  Button,
+} from "@material-ui/core";
 import Navbar from "../components/navbar";
 import axios from "axios";
-import Model from '../components/modal'
-
+import Model from "../components/modal";
 
 export default function Tutorial() {
   const [tutorials, setTutorials] = useState([]);
   const [loading, setLoading] = useState(true);
   const [isOpen, setIsOpen] = useState(false);
+  const [body, setBody] = useState(" ");
+  const [code, setCode] = useState(" ");
+  const [title, setTitle] = useState(" ");
+  const [image, setImage] = useState(" ");
 
   useEffect(() => {
     async function fetch() {
@@ -19,72 +29,89 @@ export default function Tutorial() {
     }
     fetch();
   }, []);
-  function handleDialog(){
-    setIsOpen(!isOpen)
-  }
+  const handleDialogOpen = () => {
+    setIsOpen(true);
+  };
+  const handleDialogClose = () => {
+    setIsOpen(false);
+  };
 
-
-  if(isOpen)
-  {
-      return(
-          <Model
-            isOpen = {isOpen}
-          
-          />
-      )
+  if (isOpen) {
+    return (
+      <Model
+        isOpen={isOpen}
+        title={title}
+        body={body}
+        ide={code}
+        image={image}
+        handleClose={handleDialogClose}
+      />
+    );
   }
 
   if (loading === false) {
+    console.log(tutorials);
     return (
-      <div style = {{backgroundColor:'#32354a', display:'flex', height:'100vh', flexDirection:'column'}}>
+      <div
+        style={{
+          backgroundColor: "#32354a",
+          display: "flex",
+          height: "100vh",
+          flexDirection: "column",
+        }}
+      >
         <Navbar style={{ display: "flex" }} />
 
-        <Grid style = {{paddingTop:'6rem', paddingLeft:'2rem'}}container >
-        {tutorials.map((tutorial) => {
-          return (
-            
-            <Grid item xs = {2} style = {{paddingBottom:'2rem'}}>
-                <ButtonBase onClick = {() => {
-
-                }}>
-                        <Card style = {{width: "15rem", height: '20rem', display:'flex', flexDirection:'row'}}>
-                        <CardContent
+        <Grid style={{ paddingTop: "6rem", paddingLeft: "2rem" }} container>
+          {tutorials.map((tutorial) => {
+            return (
+              <Grid item xs={2} style={{ paddingBottom: "2rem" }}>
+                <ButtonBase
+                  onClick={(event) => {
+                    handleDialogOpen();
+                    setTitle(tutorial.title);
+                    setBody(tutorial.body);
+                    setCode(tutorial.code);
+                    setImage(tutorial.output);
+                  }}
+                >
+                  <Card
+                    style={{
+                      width: "15rem",
+                      height: "20rem",
+                      display: "flex",
+                      flexDirection: "row",
+                    }}
+                  >
+                    <CardContent
+                      style={{
+                        height: "100%",
+                      }}
+                    >
+                      <Typography
+                        variant="h5"
                         style={{
-                            height: "100%",
+                          fontSize: "1em",
+                          display: "flex",
+                          justifyContent: "left",
                         }}
-                        >
-                        <img
-                            style={{
-                            borderRadius: 5,
-                            borderWidth: 10,
-                            borderColor: "white",
-                            height: "1rem",
-                            width: "1rem",
-                            }}
-                        ></img>
-                        <Typography
-                            variant="h5"
-                            style={{
-                            fontSize: "2em",
-                            display: "flex",
-                            justifyContent: "left",
-                            }}
-                        ></Typography>
-                        <Typography
-                            variant="h5"
-                            style={{
-                            fontSize: "1em",
-                            marginTop: "3vh",
-                            }}
-                        ></Typography>
-                        </CardContent>
-                    </Card>
-                </ButtonBase>   
-            </Grid>
-          );
-        })}
+                      >
+                        {tutorial.title}!
+                      </Typography>
+                      <Typography
+                        variant="h5"
+                        style={{
+                          fontSize: "1em",
+                          marginTop: "3vh",
+                        }}
+                      ></Typography>
+                    </CardContent>
+                  </Card>
+                </ButtonBase>
+              </Grid>
+            );
+          })}
         </Grid>
-       
       </div>
     );
   } else {
