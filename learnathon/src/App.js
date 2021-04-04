e4f30973e92010b4aa0d3276c0ca8af25bf91eb0
Kshitij Kapoor <kshitijkapoor8@gmail.com>
2021-04-04 12:59:28 -0500

    a

--- learnathon/src/App.js
+++ learnathon/src/App.js
@@ -1,16 +1,16 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import Login from "./pages/login";
 import Signup from "./pages/signup";
 import Home from "./pages/home";
+import Content from './pages/content';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./index.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/content" component={Content} exact />
         <Route path="/" component={Home} exact />
         <Route path="/login" component={Login} exact />
         <Route path="/signup" component={Signup} exact />
---
+++ learnathon/src/pages/content.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {
+  Col,
+  Container,
+  Row,
+  Carousel,
+  Jumbotron,
+  Button,
+} from "react-bootstrap";
+import { MDBFooter } from "mdbreact";
+import Mac from "../images/mac.jpg";
+function Content() {
+  return (
+    <div>
+        <iframe src="https://trinket.io/embed/python/39e642587a" width="100%" height="356" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe>   
+    </div>
+  );
+}
+
+export default Content;
