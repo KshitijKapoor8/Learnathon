654c80c34f6158a70b18f181bc5e0efb76dc53b1
AdeebIsmail <adeebi5412@gmail.com>
2021-04-04 13:51:31 -0500

    code

--- learnathon/src/App.js
+++ learnathon/src/App.js
@@ -2,9 +2,12 @@ import React from "react";
 import Login from "./pages/login";
 import Signup from "./pages/signup";
 import Home from "./pages/home";
-import Content from './pages/content';
+import Content from "./pages/content";
+import Page from "./pages/page";
+import View from "./pages/view";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
@@ -14,6 +17,8 @@ function App() {
         <Route path="/" component={Home} exact />
         <Route path="/login" component={Login} exact />
         <Route path="/signup" component={Signup} exact />
+        <Route path="/page" component={Page} exact />
+        <Route path="/view" component={View} exact />
       </Switch>
     </Router>
   );
--- learnathon/src/pages/content.js
+++ learnathon/src/pages/content.js
@@ -1,18 +1,18 @@
 import React from "react";
-import {
-  Col,
-  Container,
-  Row,
-  Carousel,
-  Jumbotron,
-  Button,
-} from "react-bootstrap";
-import { MDBFooter } from "mdbreact";
-import Mac from "../images/mac.jpg";
+
 function Content() {
   return (
     <div>
-        <iframe src="https://trinket.io/embed/python/39e642587a" width="100%" height="356" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe>   
+      <iframe
+        id="myFrame"
+        src="https://trinket.io/embed/python/4a9a016b16"
+        width="100%"
+        height="356"
+        frameborder="0"
+        marginwidth="0"
+        marginheight="0"
+        allowfullscreen
+      ></iframe>
     </div>
   );
 }
