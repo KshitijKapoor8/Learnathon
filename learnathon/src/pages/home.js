580b1e7e3e685f3f841ab913215be863ae9fc78a
AdeebIsmail <adeebi5412@gmail.com>
2021-04-04 12:59:42 -0500

    jf

--- learnathon/src/pages/home.js
+++ learnathon/src/pages/home.js
@@ -14,34 +14,64 @@ function Home() {
     <div>
       <Container fluid>
         <Row>
-          <Col>
-            <Carousel>
-              <Carousel.Item>
-                <img className="d-block w-100" src={Mac} height="900" />
-              </Carousel.Item>
-            </Carousel>
-          </Col>
+          <Carousel>
+            <Carousel.Item>
+              <img className="d-block w-100" src={Mac} height="900" />
+            </Carousel.Item>
+          </Carousel>
         </Row>
         <Row>
-          <Col>
-            <Jumbotron fluid className="jumbotron bg-dark text-white">
-              <h1>About Us</h1>
-              <p>
-                This is a simple hero unit, a simple jumbotron-style component
-                for calling extra attention to featured content or information.
-              </p>
-            </Jumbotron>
-          </Col>
+          <Jumbotron
+            fluid
+            className="jumbotron bg-white text-dark"
+            style={{ width: "100%" }}
+          >
+            <h1>About Us</h1>
+            <p>
+              This is a simple hero unit, a simple jumbotron-style component for
+              calling extra attention to featured content or information.
+            </p>
+          </Jumbotron>
         </Row>
         <Row>
-          <Col>
-            <MDBFooter color="brown">
-              <div className="footer-copyright text-center py-3">
-                &copy; {new Date().getFullYear()} Copyright:{" "}
-                <a href="https://www.mdbootstrap.com"> Pylearn </a>
-              </div>
-            </MDBFooter>{" "}
-          </Col>
+          <Jumbotron
+            fluid
+            className="jumbotron  text-dark"
+            style={{ width: "100%", backgroundColor: "F1F1F1" }}
+          >
+            <h1>About Us</h1>
+            <p>
+              This is a simple hero unit, a simple jumbotron-style component for
+              calling extra attention to featured content or information.
+            </p>
+          </Jumbotron>
+        </Row>
+        <Row>
+          <Jumbotron
+            fluid
+            className="jumbotron bg-dark text-white"
+            style={{ width: "100%" }}
+          >
+            <h1>About Us</h1>
+            <p>
+              This is a simple hero unit, a simple jumbotron-style component for
+              calling extra attention to featured content or information.
+            </p>
+          </Jumbotron>
+        </Row>
+        <Row>
+          <footer
+            style={{
+              width: "100%",
+              backgroundColor: "#2E2E2E",
+              color: "white",
+            }}
+          >
+            <div className="footer-copyright text-center py-3">
+              &copy; {new Date().getFullYear()} Copyright:{" "}
+              <a href="https://www.mdbootstrap.com"> Pylearn </a>
+            </div>
+          </footer>{" "}
         </Row>
       </Container>
     </div>
